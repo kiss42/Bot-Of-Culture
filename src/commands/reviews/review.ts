20e77feea78005553014a085bd0f82598406c8e1
Arik Smith <ams382@case.edu>
2022-11-13 14:43:41 -0500

    refactor common review commands to subcommands

---
+++ src/commands/reviews/review.ts
@@ -0,0 +1,40 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
+import { replyWithMovieResults } from './movies/utils'
+import { handleSubcommand } from '../utils/helpers'
+
+const command = {
+  data: new SlashCommandBuilder()
+    .setName('review')
+    .setDescription('Leave a new review')
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName('movie')
+        .setDescription('Review a movie')
+        .addStringOption((option) =>
+          option
+            .setName('title')
+            .setDescription('The title of the movie you wish to review')
+            .setRequired(true),
+        ),
+    ),
+  execute: (interaction: ChatInputCommandInteraction) =>
+    handleSubcommand(interaction, subcommandExecutors),
+}
+
+const subcommandExecutors = {
+  movie: startMovieReview,
+}
+
+async function startMovieReview(interaction: ChatInputCommandInteraction) {
+  const commandPrefix = 'reviewMovie'
+  const additionalMessage =
+    '*If already reviewed, you will be updating your previous score.*'
+  await replyWithMovieResults(
+    interaction,
+    commandPrefix,
+    additionalMessage,
+    true,
+  )
+}
+
+export default command
